32b49b9969cdc63adb554ccc61bbb78c539928dd
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-06-28 15:18:20 -0700

    Test debug menu

--- src/debug.c
+++ src/debug.c
@@ -4,33 +4,43 @@
 #include "list_menu.h"
 #include "main.h"
 #include "menu.h"
+#include "overworld.h"
+#include "pokemon.h"
 #include "script.h"
 #include "sound.h"
 #include "strings.h"
 #include "task.h"
+#include "constants/maps.h"
+#include "constants/moves.h"
 #include "constants/songs.h"
+#include "constants/species.h"
 
 #define DEBUG_MAIN_MENU_HEIGHT 2
-#define DEBUG_MAIN_MENU_WIDTH 7
+#define DEBUG_MAIN_MENU_WIDTH 6
 
 void Debug_ShowMainMenu(void);
 static void Debug_DestroyMainMenu(u8);
+static void DebugAction_Test(u8);
 static void DebugAction_Cancel(u8);
 static void DebugTask_HandleMainMenuInput(u8);
 
 enum {
+    DEBUG_MENU_ITEM_TEST,
     DEBUG_MENU_ITEM_CANCEL,
 };
 
+static const u8 gDebugText_Test[] = _("Test");
 static const u8 gDebugText_Cancel[] = _("Cancel");
 
 static const struct ListMenuItem sDebugMenuItems[] =
 {
+    [DEBUG_MENU_ITEM_TEST] = {gDebugText_Test, DEBUG_MENU_ITEM_TEST},
     [DEBUG_MENU_ITEM_CANCEL] = {gDebugText_Cancel, DEBUG_MENU_ITEM_CANCEL}
 };
 
 static void (*const sDebugMenuActions[])(u8) =
 {
+    [DEBUG_MENU_ITEM_TEST] = DebugAction_Test,
     [DEBUG_MENU_ITEM_CANCEL] = DebugAction_Cancel
 };
 
@@ -74,6 +84,7 @@ void Debug_ShowMainMenu(void) {
 
     // create window
     windowId = AddWindow(&sDebugMenuWindowTemplate);
+    PutWindowTilemap(windowId);
     DrawStdWindowFrame(windowId, FALSE);
 
     // create list menu
@@ -119,6 +130,15 @@ static void DebugTask_HandleMainMenuInput(u8 taskId)
 
 static void DebugAction_Cancel(u8 taskId)
 {
+    Debug_DestroyMainMenu(taskId);
+}
+
+static void DebugAction_Test(u8 taskId)
+{
+    u16 monData;
+
+    PlaySE(MUS_RG_FAN5);
+    CreateMon(&gPlayerParty[0], SPECIES_ZIGZAGOON, 7, 0x20, FALSE, 0, FALSE, 0);
     Debug_DestroyMainMenu(taskId);
 }
 
