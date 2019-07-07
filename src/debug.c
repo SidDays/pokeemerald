4ed69434f31df9198fc3df8331a6bbc01a4f1ca8
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-07-06 21:29:18 -0700

    Sample Debug Menu

--- src/debug.c
+++ src/debug.c
@@ -1,46 +1,54 @@
 #if DEBUG
 
 #include "global.h"
+#include "item.h"
 #include "list_menu.h"
 #include "main.h"
 #include "menu.h"
 #include "overworld.h"
 #include "pokemon.h"
+#include "random.h"
 #include "script.h"
 #include "sound.h"
 #include "strings.h"
 #include "task.h"
+#include "constants/items.h"
 #include "constants/maps.h"
 #include "constants/moves.h"
 #include "constants/songs.h"
 #include "constants/species.h"
 
-#define DEBUG_MAIN_MENU_HEIGHT 2
-#define DEBUG_MAIN_MENU_WIDTH 6
+#define DEBUG_MAIN_MENU_HEIGHT 3
+#define DEBUG_MAIN_MENU_WIDTH 10
 
 void Debug_ShowMainMenu(void);
 static void Debug_DestroyMainMenu(u8);
 static void DebugAction_Test(u8);
+static void DebugAction_RandomPokemon(u8);
 static void DebugAction_Cancel(u8);
 static void DebugTask_HandleMainMenuInput(u8);
 
 enum {
     DEBUG_MENU_ITEM_TEST,
+    DEBUG_MENU_ITEM_RANDOM_POKEMON,
     DEBUG_MENU_ITEM_CANCEL,
 };
 
 static const u8 gDebugText_Test[] = _("Test");
+static const u8 gDebugText_RandomPokemon[] = _("Random {PKMN}");
 static const u8 gDebugText_Cancel[] = _("Cancel");
 
 static const struct ListMenuItem sDebugMenuItems[] =
 {
     [DEBUG_MENU_ITEM_TEST] = {gDebugText_Test, DEBUG_MENU_ITEM_TEST},
+    [DEBUG_MENU_ITEM_RANDOM_POKEMON] = {gDebugText_RandomPokemon, DEBUG_MENU_ITEM_RANDOM_POKEMON},
     [DEBUG_MENU_ITEM_CANCEL] = {gDebugText_Cancel, DEBUG_MENU_ITEM_CANCEL}
 };
 
 static void (*const sDebugMenuActions[])(u8) =
 {
     [DEBUG_MENU_ITEM_TEST] = DebugAction_Test,
+    [DEBUG_MENU_ITEM_RANDOM_POKEMON] = DebugAction_RandomPokemon,
     [DEBUG_MENU_ITEM_CANCEL] = DebugAction_Cancel
 };
 
@@ -84,7 +92,7 @@ void Debug_ShowMainMenu(void) {
 
     // create window
     windowId = AddWindow(&sDebugMenuWindowTemplate);
-    PutWindowTilemap(windowId);
+    SetStandardWindowBorderStyle(windowId, 0);
     DrawStdWindowFrame(windowId, FALSE);
 
     // create list menu
@@ -135,11 +143,24 @@ static void DebugAction_Cancel(u8 taskId)
 
 static void DebugAction_Test(u8 taskId)
 {
-    u16 monData;
-
-    PlaySE(MUS_RG_FAN5);
-    CreateMon(&gPlayerParty[0], SPECIES_ZIGZAGOON, 7, 0x20, FALSE, 0, FALSE, 0);
+    AddBagItem(ITEM_RARE_CANDY, 99);
     Debug_DestroyMainMenu(taskId);
 }
 
+static void DebugAction_RandomPokemon(u8 taskId)
+{
+    struct Pokemon mon;
+    u16 species = Random() % NUM_SPECIES + 1;
+    u8 level = Random() % 100 + 1;
+    // u16 itemId = ITEM_MENTAL_HERB;
+    // u8 heldItem[2];
+
+    PlaySE(MUS_RG_FAN5);
+    // heldItem[0] = itemId;
+    // heldItem[1] = itemId >> 8;
+    // SetMonData(&mon, MON_DATA_HELD_ITEM, heldItem);
+    CreateMon(&mon, species, level, 0x20, FALSE, 0, FALSE, 0);
+    GiveMonToPlayer(&mon);
+    Debug_DestroyMainMenu(taskId);
+}
 #endif
